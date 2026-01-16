b79f673fe097dc763b3f56616ddfb57345990f39
Khoubaib69 <120632806+Khoubaib69@users.noreply.github.com>
2026-01-16 09:47:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import pandas as pd
+import pandas 
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:" )
 st.write(
@@ -28,7 +29,9 @@ if ingredients_list :
     ingredients_string = ''
     for fruit_chosen in ingredients_list :
         ingredients_string += fruit_chosen + ' '
-        import requests
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
